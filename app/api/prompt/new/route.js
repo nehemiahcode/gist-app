0ddc28949d6e77d2a0dfa40a043a7d228a3e3ef9
nehemiahcode <codehubby@gmail.com>
2023-12-13 09:25:52 +0100

    update

--- app/api/prompt/new/route.js
+++ app/api/prompt/new/route.js
@@ -13,7 +13,7 @@ export const POST = async (request) => {
       createdAt,
     });
 
-    await newPost.create();
+    await newPost.save();
     return new Response(JSON.stringify(newPost), { status: 201 });
   } catch (error) {
     return new Response("Failed to create a new post", { status: 500 });
