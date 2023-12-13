611accef32c46bfed087a13526d011c13b457272
nehemiahcode <codehubby@gmail.com>
2023-12-13 07:45:04 +0100

    bug fixing

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -101,7 +101,7 @@ const Feed = () => {
 
   const fetchPosts = async () => {
     try {
-      const response = await fetch(`/api/prompt?timestamp=${Date.now()}`);
+      const response = await fetch(`/api/prompt`, {cache:"no-cache"});
       const data = await response.json();
 
       setAllPosts(data);
